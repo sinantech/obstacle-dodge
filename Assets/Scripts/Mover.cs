7b84c6aa1651c8c0f5b2a17c828e497703246312
sinantech <sinancemozer@gmail.com>
2025-09-01 16:11:09 +0300

    v0.1 Learning Start Over

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,12 +5,12 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0.01f, 0f, 0f);
     }
 }
