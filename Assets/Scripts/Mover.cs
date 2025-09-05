cc2228f1289c930a41eabd60507e753338a44e9b
sinantech <sinancemozer@gmail.com>
2025-09-05 20:34:05 +0300

    v0.6 Variables

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    float xValue = 0f;
+    float yValue = 0.5f;
+    float zValue = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0f, 0f);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
